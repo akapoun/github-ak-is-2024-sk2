9e5c4646c504002ef6857089f12a9fa9d06c92f3
Antonín Kapoun <antonin.kapoun@tul.cz>
2024-10-10 13:32:01 +0200

    #1 základní program s možností opakování

---
+++ IS- Projekty/program001-vypis-rady/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program {
+
+    static void Main() {
+
+        // chci, aby se program opakoval po stisku klávesy "a"
+        string again = "a";
+        while(again == "a") {
+            Console.Clear();
+            Console.WriteLine("*****************************");
+            Console.WriteLine("********* Výpis řady ********");
+            Console.WriteLine("*****************************");
+            Console.WriteLine("******* Antonín Kapoun ******");
+            Console.WriteLine("*****************************\n\n");
+            Console.WriteLine();
+
+            // Vstup od uživatel - TO-DO
+
+            // Logika pro výpis řady - TO-DO
+
+
+            // Opakování programu
+            Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+            again = Console.ReadLine();
+        }
+
+
+    }
+
+
+}
